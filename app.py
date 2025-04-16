6ffafc5db56ffa994f111b9d9ccdd48e5ac2450d
Fian-Darlingo <wolsamsandy@gmail.com>
2025-04-16 17:18:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+import joblib
+
+app = Flask(__name__)
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
